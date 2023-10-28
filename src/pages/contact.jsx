ad95698acd8a39ca7112933cf022965c6d8094d9
Fachr1Ahmad <fachriahmad1401@gmail.com>
2023-10-28 23:56:54 +0700

    second commit

---
+++ src/pages/contact.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export const Contact = () => {
+  const contactStyle = {
+    backgroundColor: "#f0f0f0",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+  };
+
+  return (
+    <div style={contactStyle}>
+      <h2>Contact Us</h2>
+      <p>Email: fachri.120140124@student.itera.ac.id</p>
+      <p>Phone: (+62) 853-456-7890</p>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/products.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.png";
+import product9 from "./assets/products/9.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "IPhone",
+    price: 1567.69,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Macbook Pro 2022 (M1)",
+    price: 1410.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "iPad Pro (Generasi ke-6)",
+    price: 940.59,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "AirPods Max",
+    price: 595.68,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "AirPods Pro",
+    price: 206.88,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Apple Watch Series 7",
+    price: 344.84,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "AirTag Loop",
+    price: 31.29,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Apple USB-C Woven Charge Cable (1m)",
+    price: 25.02,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "iPhone 15 Plus Clear Case with MagSafe",
+    price: 62.65,
+    productImage: product9,
+  },
+];
